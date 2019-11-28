8758c09c6cb4ab788e233bdecb34fe2fe31ce47f
Rafael Dos Santos <raphael_979@hotmail.com>
2019-11-28 14:11:51 -0300

    Diferença entre var e let

---
+++ letandvar.js
@@ -0,0 +1,11 @@
+console.log(' ______________________ Diferença entre var e let______________________');
+var nome = 'Rafael';     //No caso no var posso redeclarar a variavel nome
+var nome = 'Rafael dos santos';
+console.log(nome);
+console.log(' ______________________ Usando o let______________________');
+let nome1 = 'RAFINHA';  //No let não posso declarar o nome1 duas vezes,dá erro no programa
+//*let nome1 = 'RAFINHA';
+console.log(nome1);
+
+
+
